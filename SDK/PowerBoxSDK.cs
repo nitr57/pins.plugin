cb70c8287828ee56a5bff4283c5fa4dec9961d39
Nico <nitro.mail@web.de>
2026-02-27 11:12:54 +0100

    buzzer P/Invoke

--- SDK/PowerBoxSDK.cs
+++ SDK/PowerBoxSDK.cs
@@ -366,6 +366,9 @@ namespace NINA.PINS.SDK
         public static extern PB_ERROR_TYPE PBGetVersion(int id, out PB_VERSION version);
 
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern PB_ERROR_TYPE PBBeep(int id, int volume, int duration_ns);
+
+        [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern PB_ERROR_TYPE PBRestart(int id);
 
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
